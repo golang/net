9d7c92fab0310b4671a5b5a992382c327ee93869
Mikio Hara <mikioh.mikioh@gmail.com>
2012-09-29 19:29:39 +0900

    go.net/spdy: fix typo
    R=golang-dev, dchest
    CC=golang-dev
    https://golang.org/cl/6595043

--- spdy/types.go
+++ spdy/types.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package spdy implements SPDY protocol which is descirbed in
+// Package spdy implements SPDY protocol which is described in
 // draft-mbelshe-httpbis-spdy-00.
 //
 // http://tools.ietf.org/html/draft-mbelshe-httpbis-spdy-00
