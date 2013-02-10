5a456bdb1f2dfb79f915e135bd14f6aef78ad827
Mikio Hara <mikioh.mikioh@gmail.com>
2013-02-10 10:45:18 +0900

    go.net/proxy: make proxy tests able to run with host protocol stack
    R=golang-dev, dave, agl
    CC=golang-dev
    https://golang.org/cl/7314047

--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -13,43 +13,41 @@ import (
 	"testing"
 )
 
-type testFromURLDialer struct {
-	network, addr string
-}
-
-func (t *testFromURLDialer) Dial(network, addr string) (net.Conn, error) {
-	t.network = network
-	t.addr = addr
-	return nil, t
-}
-
-func (t *testFromURLDialer) Error() string {
-	return "testFromURLDialer " + t.network + " " + t.addr
-}
-
 func TestFromURL(t *testing.T) {
-	u, err := url.Parse("socks5://user:password@1.2.3.4:5678")
+	endSystem, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
-		t.Fatalf("failed to parse URL: %s", err)
+		t.Fatalf("net.Listen failed: %v", err)
 	}
-
-	tp := &testFromURLDialer{}
-	proxy, err := FromURL(u, tp)
+	defer endSystem.Close()
+	gateway, err := net.Listen("tcp", "127.0.0.1:0")
 	if err != nil {
-		t.Fatalf("FromURL failed: %s", err)
+		t.Fatalf("net.Listen failed: %v", err)
 	}
+	defer gateway.Close()
 
-	conn, err := proxy.Dial("tcp", "example.com:80")
-	if conn != nil {
-		t.Error("Dial unexpected didn't return an error")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go socks5Gateway(t, gateway, endSystem, socks5Domain, &wg)
+
+	url, err := url.Parse("socks5://user:password@" + gateway.Addr().String())
+	if err != nil {
+		t.Fatalf("url.Parse failed: %v", err)
 	}
-	if tp, ok := err.(*testFromURLDialer); ok {
-		if tp.network != "tcp" || tp.addr != "1.2.3.4:5678" {
-			t.Errorf("Dialer connected to wrong host. Wanted 1.2.3.4:5678, got: %v", tp)
-		}
+	proxy, err := FromURL(url, Direct)
+	if err != nil {
+		t.Fatalf("FromURL failed: %v", err)
+	}
+	_, port, err := net.SplitHostPort(endSystem.Addr().String())
+	if err != nil {
+		t.Fatalf("net.SplitHostPort failed: %v", err)
+	}
+	if c, err := proxy.Dial("tcp", "localhost:"+port); err != nil {
+		t.Fatalf("FromURL.Dial failed: %v", err)
 	} else {
-		t.Errorf("Unexpected error from Dial: %s", err)
+		c.Close()
 	}
+
+	wg.Wait()
 }
 
 func TestSOCKS5(t *testing.T) {
@@ -64,9 +62,9 @@ func TestSOCKS5(t *testing.T) {
 	}
 	defer gateway.Close()
 
-	wg := &sync.WaitGroup{}
-	go socks5Gateway(t, gateway, endSystem, wg)
+	var wg sync.WaitGroup
 	wg.Add(1)
+	go socks5Gateway(t, gateway, endSystem, socks5IP4, &wg)
 
 	proxy, err := SOCKS5("tcp", gateway.Addr().String(), nil, Direct)
 	if err != nil {
@@ -81,40 +79,64 @@ func TestSOCKS5(t *testing.T) {
 	wg.Wait()
 }
 
-func socks5Gateway(t *testing.T, gateway, endSystem net.Listener, wg *sync.WaitGroup) {
+func socks5Gateway(t *testing.T, gateway, endSystem net.Listener, typ byte, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	c, err := gateway.Accept()
 	if err != nil {
-		t.Fatalf("net.Listener.Accept failed: %v", err)
+		t.Errorf("net.Listener.Accept failed: %v", err)
+		return
 	}
 	defer c.Close()
 
 	b := make([]byte, 32)
-	if _, err := io.ReadFull(c, b[:3]); err != nil {
-		t.Fatalf("net.Conn.Read failed: %v", err)
+	var n int
+	if typ == socks5Domain {
+		n = 4
+	} else {
+		n = 3
+	}
+	if _, err := io.ReadFull(c, b[:n]); err != nil {
+		t.Errorf("io.ReadFull failed: %v", err)
+		return
 	}
 	if _, err := c.Write([]byte{socks5Version, socks5AuthNone}); err != nil {
-		t.Fatalf("net.Conn.Write failed: %v", err)
+		t.Errorf("net.Conn.Write failed: %v", err)
+		return
 	}
-	if _, err := io.ReadFull(c, b[:10]); err != nil {
-		t.Fatalf("net.Conn.Read failed: %v", err)
+	if typ == socks5Domain {
+		n = 16
+	} else {
+		n = 10
 	}
-	if b[0] != socks5Version || b[1] != socks5Connect || b[2] != 0x00 || b[3] != socks5IP4 {
-		t.Fatalf("got an unexpected packet: %v, %v, %v, %v", b[0], b[1], b[2], b[3])
+	if _, err := io.ReadFull(c, b[:n]); err != nil {
+		t.Errorf("io.ReadFull failed: %v", err)
+		return
 	}
-	copy(b[:4], []byte{socks5Version, 0x00, 0x00, socks5IP4})
+	if b[0] != socks5Version || b[1] != socks5Connect || b[2] != 0x00 || b[3] != typ {
+		t.Errorf("got an unexpected packet: %#02x %#02x %#02x %#02x", b[0], b[1], b[2], b[3])
+		return
+	}
+	if typ == socks5Domain {
+		copy(b[:5], []byte{socks5Version, 0x00, 0x00, socks5Domain, 9})
+		b = append(b, []byte("localhost")...)
+	} else {
+		copy(b[:4], []byte{socks5Version, 0x00, 0x00, socks5IP4})
+	}
 	host, port, err := net.SplitHostPort(endSystem.Addr().String())
 	if err != nil {
-		t.Fatalf("net.SplitHostPort failed: %v", err)
+		t.Errorf("net.SplitHostPort failed: %v", err)
+		return
 	}
 	b = append(b, []byte(net.ParseIP(host).To4())...)
 	p, err := strconv.Atoi(port)
 	if err != nil {
-		t.Fatalf("strconv.Atoi failed: %v", err)
+		t.Errorf("strconv.Atoi failed: %v", err)
+		return
 	}
 	b = append(b, []byte{byte(p >> 8), byte(p)}...)
 	if _, err := c.Write(b); err != nil {
-		t.Fatalf("net.Conn.Write failed: %v", err)
+		t.Errorf("net.Conn.Write failed: %v", err)
+		return
 	}
 }
