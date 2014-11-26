41bf2334818697e373f21719dcf537b1d9896a93
Mikio Hara <mikioh.mikioh@gmail.com>
2014-11-27 07:14:33 +0900

    x/net/ipv4: remove unnecessary stubs
    LGTM=iant
    R=iant
    CC=golang-codereviews
    https://golang.org/cl/181200043

--- ipv4/sockopt_stub.go
+++ ipv4/sockopt_stub.go
@@ -6,24 +6,6 @@
 
 package ipv4
 
-import "net"
-
-func getInt(fd int, opt *sockOpt) (int, error) {
-	return 0, errOpNoSupport
-}
-
 func setInt(fd int, opt *sockOpt, v int) error {
-	return errOpNoSupport
-}
-
-func getInterface(fd int, opt *sockOpt) (*net.Interface, error) {
-	return nil, errOpNoSupport
-}
-
-func setInterface(fd int, opt *sockOpt, ifi *net.Interface) error {
-	return errOpNoSupport
-}
-
-func setGroup(fd int, opt *sockOpt, ifi *net.Interface, ip net.IP) error {
 	return errOpNoSupport
 }
