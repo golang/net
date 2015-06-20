10576091dc82c9c109dddfb5ed77bdbbc87a9af8
Mikio Hara <mikioh.mikioh@gmail.com>
2015-06-20 09:15:12 +0900

    ipv6: simplify init function for freebsd
    Change-Id: I515bdaf3f30b00d61c341295e549bb4c2163bd54
    Reviewed-on: https://go-review.googlesource.com/11293
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- ipv6/sys_freebsd.go
+++ ipv6/sys_freebsd.go
@@ -50,8 +50,8 @@ var (
 func init() {
 	if runtime.GOOS == "freebsd" && runtime.GOARCH == "386" {
 		archs, _ := syscall.Sysctl("kern.supported_archs")
-		for _, s := range strings.Split(archs, " ") {
-			if strings.TrimSpace(s) == "amd64" {
+		for _, s := range strings.Fields(archs) {
+			if s == "amd64" {
 				freebsd32o64 = true
 				break
 			}
