09f95eed15c1248a71c16103fe9d573fd7e5d6a0
Mikio Hara <mikioh.mikioh@gmail.com>
2015-10-26 12:18:19 +0900

    ipv4: simplify log message format
    This change replaces the existing log format separated by commas and
    spaces with space-separated one.
    
    Also removes unnecessary log output in test.
    
    Change-Id: I1bf2b8c463db9174b2ac562533cdd89b72ed39de
    Reviewed-on: https://go-review.googlesource.com/16322
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- ipv4/header.go
+++ ipv4/header.go
@@ -46,7 +46,7 @@ func (h *Header) String() string {
 	if h == nil {
 		return "<nil>"
 	}
-	return fmt.Sprintf("ver: %v, hdrlen: %v, tos: %#x, totallen: %v, id: %#x, flags: %#x, fragoff: %#x, ttl: %v, proto: %v, cksum: %#x, src: %v, dst: %v", h.Version, h.Len, h.TOS, h.TotalLen, h.ID, h.Flags, h.FragOff, h.TTL, h.Protocol, h.Checksum, h.Src, h.Dst)
+	return fmt.Sprintf("ver=%d hdrlen=%d tos=%#x totallen=%d id=%#x flags=%#x fragoff=%#x ttl=%d proto=%d cksum=%#x src=%v dst=%v", h.Version, h.Len, h.TOS, h.TotalLen, h.ID, h.Flags, h.FragOff, h.TTL, h.Protocol, h.Checksum, h.Src, h.Dst)
 }
 
 // Marshal returns the binary encoding of the IPv4 header h.
