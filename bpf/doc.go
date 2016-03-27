3c208088b61813bc5f31286f8117d6f1baa781ab
David Anderson <danderson@google.com>
2016-03-27 12:58:12 -0700

    bpf: new package to assemble and disassemble Berkeley Packet Filter programs.
    The package currently implements the operations defined by
    https://www.kernel.org/doc/Documentation/networking/filter.txt , which
    comprises the base BPF virtual machine plus the Linux kernel's extension
    opcodes.
    
    Updates golang/go#14982
    
    Change-Id: Iafb43d80e067040e60465a9bfb7d5f2ca90cc2ae
    Reviewed-on: https://go-review.googlesource.com/21212
    Reviewed-by: Mikio Hara <mikioh.mikioh@gmail.com>

---
+++ bpf/doc.go
@@ -0,0 +1,11 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package bpf implements marshalling and unmarshalling of programs
+// for the Berkeley Packet Filter virtual machine.
+//
+// TODO: brief overview of the BPF virtual machine (registers, scratch, packet access, execution constraints)
+//
+// TODO: simple BPF program examples
+package bpf // import "golang.org/x/net/bpf"
