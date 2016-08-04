7c62cfdcccc65f87b0120ec841012ba816fc1aec
Brad Fitzpatrick <bradfitz@golang.org>
2016-08-04 20:59:57 +0000

    http2: add missing import path declaration
    Change-Id: Ic37b6b84bfc695cfb1ca7cf8e7da52c801e6d236
    Reviewed-on: https://go-review.googlesource.com/25500
    Reviewed-by: Chris Broadfoot <cbro@golang.org>

--- http2/http2.go
+++ http2/http2.go
@@ -13,7 +13,8 @@
 // See https://http2.github.io/ for more information on HTTP/2.
 //
 // See https://http2.golang.org/ for a test server running this code.
-package http2
+//
+package http2 // import "golang.org/x/net/http2"
 
 import (
 	"bufio"
