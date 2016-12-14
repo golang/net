f483ac36322f58b5dae6e2b4fda70759083fae28
Mikio Hara <mikioh.mikioh@gmail.com>
2016-12-15 07:23:17 +0900

    ipv6: add missing BUGS section
    Change-Id: I42e83ca311b499459c4d6fa75984be3e86da6a22
    Reviewed-on: https://go-review.googlesource.com/34433
    Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

--- ipv6/doc.go
+++ ipv6/doc.go
@@ -239,3 +239,5 @@
 // In the fallback case, ExcludeSourceSpecificGroup and
 // IncludeSourceSpecificGroup may return an error.
 package ipv6 // import "golang.org/x/net/ipv6"
+
+// BUG(mikio): This package is not implemented on NaCl and Plan 9.
