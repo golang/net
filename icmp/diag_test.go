d6388d9676bbd1fc7239690a5d509a4634e06a93
Mikio Hara <mikioh.public.networking@gmail.com>
2018-12-27 04:18:42 +0900

    icmp: don't run diagnostic tests by default, even in long mode
    Change-Id: I4eec45705348437ec2ee7eae8d0d98db0f28152d
    Reviewed-on: https://go-review.googlesource.com/c/155858
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- icmp/diag_test.go
+++ icmp/diag_test.go
@@ -6,6 +6,7 @@ package icmp_test
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -21,6 +22,8 @@ import (
 	"golang.org/x/net/ipv6"
 )
 
+var testDiag = flag.Bool("diag", false, "whether to test ICMP message exchange with external network")
+
 type diagTest struct {
 	network, address string
 	protocol         int
@@ -28,7 +31,7 @@ type diagTest struct {
 }
 
 func TestDiag(t *testing.T) {
-	if testing.Short() {
+	if !*testDiag {
 		t.Skip("avoid external network")
 	}
 
