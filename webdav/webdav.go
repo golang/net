aa7ab65e4c0d5381b77b141ffbeea0d18c25794d
Nicola Murino <nicola.murino@gmail.com>
2020-08-07 18:12:57 +0200

    webdav PUT: call Stat only if the file uploads has no previous errors
    if io.Copy or f.Close return an error we don't need to issue a Stat at
    all

--- webdav/webdav.go
+++ webdav/webdav.go
@@ -270,16 +270,16 @@ func (h *Handler) handlePut(w http.ResponseWriter, r *http.Request) (status int,
 	}
 	_, copyErr := io.Copy(f, r.Body)
 	closeErr := f.Close()
-	fi, statErr := f.Stat()
 	// TODO(rost): Returning 405 Method Not Allowed might not be appropriate.
 	if copyErr != nil {
 		return http.StatusMethodNotAllowed, copyErr
 	}
-	if statErr != nil {
-		return http.StatusMethodNotAllowed, statErr
-	}
 	if closeErr != nil {
 		return http.StatusMethodNotAllowed, closeErr
+	}
+	fi, statErr := f.Stat()
+	if statErr != nil {
+		return http.StatusMethodNotAllowed, statErr
 	}
 	etag, err := findETag(ctx, h.FileSystem, h.LockSystem, reqPath, fi)
 	if err != nil {
