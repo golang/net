5f55cee0dc0dc168ce29222f077fe7fcd4be72c5
Russ Cox <rsc@golang.org>
2021-02-19 18:54:43 -0500

    all: go fmt ./...
    Make all our package sources use Go 1.17 gofmt format
    (adding //go:build lines).
    
    Not strictly necessary but will avoid spurious changes
    as files are edited.
    
    Part of //go:build change (#41184).
    See https://golang.org/design/draft-gobuild
    
    Change-Id: I5b2b7d93424e828a3c5f76ae3f30ab825aca388e
    Reviewed-on: https://go-review.googlesource.com/c/net/+/294371
    Trust: Russ Cox <rsc@golang.org>
    Run-TryBot: Russ Cox <rsc@golang.org>
    TryBot-Result: Go Bot <gobot@golang.org>
    Reviewed-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- ipv6/defs_aix.go
+++ ipv6/defs_aix.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build ignore
 // +build ignore
 
 // +godefs map struct_in6_addr [16]byte /* in6_addr */
