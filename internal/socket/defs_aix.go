2554d15bf5f76153d3dcd2bb249d4fe4f868b9b9
Tobias Klauser <tklauser@distanz.ch>
2021-04-22 00:00:00 +0200

    internal/socket: use SizeofSockaddrInet{4,6} constants from x/sys/unix
    Use the SizeofSockaddrInet4 and SizeofSockaddrInet6 constants defined in
    the golang.org/x/sys/unix package instead of locally duplicating them.
    
    Change-Id: I7e7ea9ec6e81a0af70bbf5c7e91f61a0662228ff
    Reviewed-on: https://go-review.googlesource.com/c/net/+/312489
    Trust: Tobias Klauser <tobias.klauser@gmail.com>
    Run-TryBot: Tobias Klauser <tobias.klauser@gmail.com>
    Reviewed-by: Ian Lance Taylor <iant@golang.org>

--- internal/socket/defs_aix.go
+++ internal/socket/defs_aix.go
@@ -5,9 +5,6 @@
 //go:build ignore
 // +build ignore
 
-// +godefs map struct_in_addr [4]byte /* in_addr */
-// +godefs map struct_in6_addr [16]byte /* in6_addr */
-
 package socket
 
 /*
@@ -25,14 +22,7 @@ type mmsghdr C.struct_mmsghdr
 
 type cmsghdr C.struct_cmsghdr
 
-type sockaddrInet C.struct_sockaddr_in
-
-type sockaddrInet6 C.struct_sockaddr_in6
-
 const (
 	sizeofIovec  = C.sizeof_struct_iovec
 	sizeofMsghdr = C.sizeof_struct_msghdr
-
-	sizeofSockaddrInet  = C.sizeof_struct_sockaddr_in
-	sizeofSockaddrInet6 = C.sizeof_struct_sockaddr_in6
 )
