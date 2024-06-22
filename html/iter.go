74b75cf4831d3c7866fa3e36d912aa159d524c2a
Carlana Johnson <me@carlana.net>
2024-06-22 14:56:43 -0400

    x/net/html: add Node.All() and Node.ChildNodes()
    Fixes #62113

---
+++ html/iter.go
@@ -0,0 +1,51 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build goexperiment.rangefunc
+
+package html
+
+import "iter"
+
+// ChildNodes returns a sequence yielding the immediate children of n.
+//
+// Mutating a Node while iterating through its ChildNodes may have unexpected results.
+func (n *Node) ChildNodes() iter.Seq[*Node] {
+	return func(yield func(*Node) bool) {
+		if n == nil {
+			return
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			if !yield(c) {
+				return
+			}
+		}
+	}
+
+}
+
+func (n *Node) all(yield func(*Node) bool) bool {
+	if n == nil {
+		return true
+	}
+	if !yield(n) {
+		return false
+	}
+
+	for c := range n.ChildNodes() {
+		if !c.all(yield) {
+			return false
+		}
+	}
+	return true
+}
+
+// All returns a sequence yielding all descendents of n in depth-first pre-order.
+//
+// Mutating a Node while iterating through it or its descendents may have unexpected results.
+func (n *Node) All() iter.Seq[*Node] {
+	return func(yield func(*Node) bool) {
+		n.all(yield)
+	}
+}
