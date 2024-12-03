d0a1049b7e05d3d6f62570a8381a22301a97ae73
Tobias Klauser <tklauser@distanz.ch>
2024-12-03 13:23:01 +0100

    route: remove unused sizeof* consts on freebsd
    The sizeofSockaddr* consts were replaced by the respective x/sys/unix
    consts by CL 632816.
    
    The sizeofIfDataFreeBSD* consts were never used since they were added.
    
    Cq-Include-Trybots: luci.golang.try:x_net-gotip-freebsd-amd64
    Change-Id: Ieb0e01353199e08659d45cdc3293e817b679fed5
    Reviewed-on: https://go-review.googlesource.com/c/net/+/633155
    Reviewed-by: Ian Lance Taylor <iant@google.com>
    Auto-Submit: Tobias Klauser <tobias.klauser@gmail.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Auto-Submit: Damien Neil <dneil@google.com>
    Reviewed-by: Damien Neil <dneil@google.com>

--- route/zsys_freebsd_arm.go
+++ route/zsys_freebsd_arm.go
@@ -13,12 +13,6 @@ const (
 	sizeofIfMsghdrFreeBSD10 = 0x70
 	sizeofIfMsghdrFreeBSD11 = 0xa8
 
-	sizeofIfDataFreeBSD7  = 0x60
-	sizeofIfDataFreeBSD8  = 0x60
-	sizeofIfDataFreeBSD9  = 0x60
-	sizeofIfDataFreeBSD10 = 0x60
-	sizeofIfDataFreeBSD11 = 0x98
-
 	sizeofRtMsghdrFreeBSD10Emu  = 0x5c
 	sizeofRtMetricsFreeBSD10Emu = 0x38
 
@@ -27,14 +21,4 @@ const (
 	sizeofIfMsghdrFreeBSD9Emu  = 0x70
 	sizeofIfMsghdrFreeBSD10Emu = 0x70
 	sizeofIfMsghdrFreeBSD11Emu = 0xa8
-
-	sizeofIfDataFreeBSD7Emu  = 0x60
-	sizeofIfDataFreeBSD8Emu  = 0x60
-	sizeofIfDataFreeBSD9Emu  = 0x60
-	sizeofIfDataFreeBSD10Emu = 0x60
-	sizeofIfDataFreeBSD11Emu = 0x98
-
-	sizeofSockaddrStorage = 0x80
-	sizeofSockaddrInet    = 0x10
-	sizeofSockaddrInet6   = 0x1c
 )
