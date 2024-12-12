ef74c9577a4005018c2303810947d2da4f5b4ca6
Hubert Grochowski <hubert.grochowski21@gmail.com>
2024-12-12 13:16:15 +0100

    http2: count error on server lost ping
    This is to unify lost ping handling in server and transport.

--- http2/server.go
+++ http2/server.go
@@ -1069,6 +1069,9 @@ func (sc *serverConn) serve(conf http2Config) {
 func (sc *serverConn) handlePingTimer(lastFrameReadTime time.Time) {
 	if sc.pingSent {
 		sc.logf("timeout waiting for PING response")
+		if f := sc.countErrorFunc; f != nil {
+			f("conn_close_lost_ping")
+		}
 		sc.conn.Close()
 		return
 	}
