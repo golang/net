dd0b200aed506ae7891bd38fb47bacfd57b5a6e0
Damien Neil <dneil@google.com>
2025-04-08 17:04:44 -0700

    quic: remove go1.21 build constraint
    The x/net go.mod now depends on go1.23.
    
    Change-Id: I24960949d84753f84f75fbdc97b7dfb92191a4d2
    Reviewed-on: https://go-review.googlesource.com/c/net/+/664295
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Jonathan Amsterdam <jba@google.com>
    Auto-Submit: Damien Neil <dneil@google.com>

--- quic/sent_packet_list.go
+++ quic/sent_packet_list.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build go1.21
-
 package quic
 
 // A sentPacketList is a ring buffer of sentPackets.
--- quic/sent_val.go
+++ quic/sent_val.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build go1.21
-
 package quic
 
 // A sentVal tracks sending some piece of information to the peer.
